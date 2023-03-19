7a571ef9d16e9c8fba5f3cc86faab3425828a201
Timea Szucs <szucstimea02@gmail.com>
2023-03-19 16:11:11 +0100

    irsz és cég adatainak lekérése

--- inndata.php
+++ inndata.php
@@ -9,10 +9,14 @@ $panzioadatok -> bindColumn("nev",$panzionev);
 $panzioadatok -> bindColumn("telszam",$panziotel);
 $panzioadatok -> bindColumn("email",$panzioemail);
 $panzioadatok -> bindColumn("kapacitas",$kapacitas);
+$panzioadatok -> bindColumn("iranyitoszam",$panzioirsz);
 $panzioadatok -> bindColumn("megye",$panziomegye);
 $panzioadatok -> bindColumn("varos",$panziovaros);
 $panzioadatok -> bindColumn("utca",$panzioutca);
 $panzioadatok -> bindColumn("hazszam",$panziohazszam);
+$panzioadatok -> bindColumn("adoszam",$panzioadoszam);
+$panzioadatok -> bindColumn("cegjegyzek",$panziocegjegyzek);
+$panzioadatok -> bindColumn("engedely",$panzioengedely);
 $panzioadatok -> bindColumn("bemutatkozas",$panziobemutatkozas);
 $panzioadatok -> fetch(PDO::FETCH_ASSOC)
 ?>
