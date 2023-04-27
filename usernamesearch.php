4b5b13f8520cc0da163804ea568e3e46f2980f0f
Csonka Csaba <csonkacsaba1986@gmail.com>
2023-04-27 23:31:18 +0200

    felhasználónév ellenőrzés módosítás bejelentkezéskor

--- usernamesearch.php
+++ usernamesearch.php
@@ -1,33 +1,13 @@
 <?php
 include 'dbconnect.php';
-// class CheckUsername extends Db{
-
-//     public function check($username){
-//         $sql ="SELECT * FROM Felhasznalok WHERE felhNev = ?";
-//         $stmt = $this->connect()->prepare($sql);
-//         $stmt->execute([$username]);
-//         $users = $stmt->fetchAll();
-
-//         foreach($users as $user){
-//             if($user == $username){
-//                 echo '
-//                 <div class="alert alert-danger alert-dismissible fade show" role="alert" id="foglaltfelhasznalonev"">
-//                 <i class="bi bi-info-circle-fill"></i> A megadott felhasználónévvel még nem regisztráltak! Kérem adjon meg másikat vagy regisztráljon itt!
-//                 <button type="button" class="close" data-dismiss="alert" id="close3">
-//                     x
-//                 </button>
-//                 </div>
-//                 ';
-//             }
-//         }
-//     }
-// }
 
 $output ="";
 if(isset($_POST["search"])){
+    $user = $_POST["search"];
     try{
-    $sql = "SELECT * FROM lodinn.felhasznalok WHERE felhasznalok.felhNev LIKE '%".$_POST["search"]."%'";
+    $sql = "SELECT * FROM lodinn.felhasznalok WHERE felhasznalok.felhNev = ?";
     $result = $conn->prepare($sql);
+    $result->bindParam(1, $user, PDO::PARAM_STR);
     $result->execute();
     if ($result ->rowCount()==0){
         $output .='
